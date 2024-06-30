ffa75e97835f09c21d412ff7abe1600522c0c936
Jefferson Ferreira <jeffcabelo@gmail.com>
2024-06-29 23:34:30 -0300

    REINICIANDO PROJETO

---
+++ TesteGitHubDesktop/src/testegithubdesktop/Teste1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testegithubdesktop;
+
+/**
+ *
+ * @author Jefferson
+ */
+public class Teste1 {
+    
+}
